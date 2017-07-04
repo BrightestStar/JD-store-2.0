d780c788a8cc00a8de6145be8409d42b503e9dc6
lucas <d2194499406@gmail.com>
2017-07-04 12:00:11 +0800

    add admin_required and client coule find product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :admin_required
 
   def index
     @products = Product.all
@@ -45,6 +47,6 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :price, :quantity)
+    params.require(:product).permit(:title, :description, :price, :quantity, :image)
   end
 end
