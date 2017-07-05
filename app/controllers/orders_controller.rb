1fb9cea3743e6e466fef928ea8e3fbddad244292
lucas <d2194499406@gmail.com>
2017-07-05 20:48:04 +0800

    add notify_cancel/ship function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -46,6 +46,12 @@ class OrdersController < ApplicationController
     redirect_to order_path(@order.token), notice: "微信支付成功"
   end
 
+  def apply_to_cancel
+    @order = Order.find_by_token(params[:id])
+    OrderMailer.apply_cancel(@order).deliver!
+    redirect_to request.referer, notice: "已提交申请"
+  end
+
   private
 
   def order_params
