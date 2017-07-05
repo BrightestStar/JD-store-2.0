1d3a6c79ac419b789bdf059de07ca6d8c55d2c5d
lucas <d2194499406@gmail.com>
2017-07-05 20:09:07 +0800

    add admin/orders/state_option button

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,4 +6,33 @@ class Admin::OrdersController < ApplicationController
   def index
     @orders = Order.order("id DESC")
   end
+
+  def show
+    @order = Order.find(params[:id])
+    @product_lists = @order.product_lists
+  end
+
+  def ship
+    @order = Order.find(params[:id])
+    @order.ship!
+    redirect_to request.referer
+  end
+
+  def shipped
+    @order = Order.find(params[:id])
+    @order.deliver!
+    redirect_to request.referer
+  end
+
+  def cancel
+    @order = Order.find(params[:id])
+    @order.cancel_order!
+    redirect_to request.referer
+  end
+
+  def return
+    @order = Order.find(params[:id])
+    @order.return_good!
+    redirect_to request.referer
+  end
 end
